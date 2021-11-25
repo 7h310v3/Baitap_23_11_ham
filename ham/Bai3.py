a2b67d627a433974c16d730ee782c07c6719572a
7h310v3 <minh92dd@gmail.com>
2021-11-25 21:14:34 +0700

    thelove

--- ham/Bai3.py
+++ ham/Bai3.py
@@ -1,19 +1,19 @@
 import Bai2
 
 def NhapN():
-    n = int(input("Nhập N: "))
-    while(n < 1):
+    x = int(input("Nhập N: "))
+    while(x < 1):
         print("Nhập sai, xin nhập lại!")
-        n = int(input("Nhập N: "))
-    return n
+        x = int(input("Nhập N: "))
+    return x
 
 def NhapThongDiep():
-    global stg
     stg = input("Nhập thông điệp muốn in ra: ")
+    return stg
 
 def main():
     n = NhapN()
-    NhapThongDiep()
+    stg = NhapThongDiep()
 
     count = 0
     print("Thông điệp muốn in ra là: ")
